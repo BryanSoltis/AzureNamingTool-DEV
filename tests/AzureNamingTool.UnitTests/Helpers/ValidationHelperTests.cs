3553058206b11531efe5811e5aee681e918bd869
Bryan Soltis <brsoltis@microsoft.com>
2025-11-03 11:01:59 -0600

    Add ResourceComponent service and ValidationHelper tests - 24 new tests
    - Add ResourceComponentServiceTests with admin filtering (5 tests)
    - Add expanded ValidationHelperTests for CheckNumeric, CheckAlphanumeric, CheckComponentLength (15 test variations)
    - Coverage: 9.15% -> 9.46% (+0.31% line, +0.11% branch)
    - Tests: 229 -> 253 (+24 tests)
    - Passing: 215 -> 239 (+24 passing)
    - Nearly at 10% coverage milestone!

--- tests/AzureNamingTool.UnitTests/Helpers/ValidationHelperTests.cs
+++ tests/AzureNamingTool.UnitTests/Helpers/ValidationHelperTests.cs
@@ -1,4 +1,5 @@
 using AzureNamingTool.Helpers;
+using AzureNamingTool.Models;
 using FluentAssertions;
 using Xunit;
 
@@ -23,5 +24,83 @@ public class ValidationHelperTests
 
         // Assert
         result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("123", true)]
+    [InlineData("456789", true)]
+    [InlineData("0", true)]
+    [InlineData("abc", false)]
+    [InlineData("123abc", false)]
+    [InlineData("", false)]
+    [InlineData("12.34", false)]
+    public void CheckNumeric_ShouldValidateNumericStrings(string value, bool expected)
+    {
+        // Act
+        var result = ValidationHelper.CheckNumeric(value);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("abc123", true)]
+    [InlineData("ABC", true)]
+    [InlineData("123", true)]
+    [InlineData("abc123xyz", true)]
+    [InlineData("abc-123", false)]
+    [InlineData("abc_123", false)]
+    [InlineData("abc 123", false)]
+    [InlineData("abc@123", false)]
+    [InlineData("abc.123", false)]
+    public void CheckAlphanumeric_ShouldValidateAlphanumericStrings(string value, bool expected)
+    {
+        // Act
+        var result = ValidationHelper.CheckAlphanumeric(value);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void CheckComponentLength_ShouldReturnTrue_WhenValueWithinRange()
+    {
+        // Arrange
+        var component = new ResourceComponent { MinLength = "3", MaxLength = "10" };
+        var value = "test";
+
+        // Act
+        var result = ValidationHelper.CheckComponentLength(component, value);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void CheckComponentLength_ShouldReturnFalse_WhenValueTooShort()
+    {
+        // Arrange
+        var component = new ResourceComponent { MinLength = "5", MaxLength = "10" };
+        var value = "abc";
+
+        // Act
+        var result = ValidationHelper.CheckComponentLength(component, value);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void CheckComponentLength_ShouldReturnFalse_WhenValueTooLong()
+    {
+        // Arrange
+        var component = new ResourceComponent { MinLength = "3", MaxLength = "5" };
+        var value = "toolongvalue";
+
+        // Act
+        var result = ValidationHelper.CheckComponentLength(component, value);
+
+        // Assert
+        result.Should().BeFalse();
     }
 }
